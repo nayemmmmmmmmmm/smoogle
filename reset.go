cfb41cb98269134fa01e54bd8b94cf335c27ac03
Arafat Hasan <nayemarafat786@gmail.com>
2026-02-18 17:13:56 +0600

    Configuring routes

--- reset.go
+++ reset.go
@@ -3,6 +3,11 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
